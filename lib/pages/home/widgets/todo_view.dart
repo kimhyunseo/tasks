20660caa13d5e1ef19be0c12548754ffede79832
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2025-12-17 22:26:25 +0900

    feat(detail): TodoView에서 만든 위젯 클릭하면 세부 위젯으로 이동 구현

--- lib/pages/home/widgets/todo_view.dart
+++ lib/pages/home/widgets/todo_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/pages/home/widgets/todo_entity.dart';
 import 'package:tasks/pages/home/widgets/todo_widget.dart';
+import 'package:tasks/pages/todo_detail/todo_detail_page.dart';
 
 class TodoView extends StatelessWidget {
   const TodoView({
@@ -20,14 +21,27 @@ class TodoView extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(12, 12, 12, 150),
       itemCount: todoList.length,
       itemBuilder: (context, index) {
-        return ToDoWidget(
-          todo: todoList[index],
-          onToggleFavorite: () {
-            onToggleFavorite(index);
+        // TODO: 질문하기, 제일 큰 부모를 제스쳐로 감싼 뒤에 onTap을 해도 안에 자식들에게 영향이 안 가는지?
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return TodoDetailPage(index: index, todoList: todoList);
+                },
+              ),
+            );
           },
-          onToggleDone: () {
-            onToggleDone(index);
-          },
+          child: ToDoWidget(
+            todo: todoList[index],
+            onToggleFavorite: () {
+              onToggleFavorite(index);
+            },
+            onToggleDone: () {
+              onToggleDone(index);
+            },
+          ),
         );
       },
     );
--- lib/pages/todo_detail/todo_detail_page.dart
+++ lib/pages/todo_detail/todo_detail_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:tasks/pages/home/widgets/todo_entity.dart';
 
 class TodoDetailPage extends StatelessWidget {
-  const TodoDetailPage({super.key});
+  const TodoDetailPage({
+    super.key,
+    required this.todoList,
+    required this.index,
+  });
+  final List<ToDoEntity> todoList;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todo Detail')),
-      body: Center(child: Text('Details of the selected Todo item.')),
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.star_rounded, size: 28),
+            ),
+          ),
+        ],
+      ),
+      body: Column(children: [Text(todoList[index].title)]),
     );
   }
 }
